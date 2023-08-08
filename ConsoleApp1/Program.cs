ef4ac294fc893f414558d2cde3e3a793ca948738
Dinesh kumar <dineshkumar.g@metamation.com>
2023-08-08 12:18:08 +0530

    Modified program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,2 @@
-﻿namespace ConsoleApp1 {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine ("Hello, World!");
+        
